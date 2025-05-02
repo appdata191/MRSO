c10d3c2cb819cf9d4b4e4abfabe99fe3814c87eb
Tuan Anh Le <tle0@mahr209-12.ad.insa-rouen.fr>
2025-05-02 17:21:58 +0200

    modified and add many things

---
+++ Part 3.2 (epsilon).R
@@ -0,0 +1,69 @@
+H1_iter <- c()
+# Instruction pour que tout le monde ait le m^eme al´ea de d´epart\
+for (eps in c(1/100, 1/20, 1/7)){
+  H0_list <- c()
+  N_list <- c(50, 100, 200, 500, 1000)
+  for (N in N_list){
+    z_iter = c()
+    lmao = c()
+    for (epoch in 1:400)
+    {
+      RNGkind(sample.kind = "Rounding")
+      prob = c(1/6 - eps, 1/6, 1/6 + eps, 1/6 + eps, 1/6, 1/6 - eps)
+      P = cumsum(prob)
+      x = (1:length(P))[runif(1)<P][1]
+      
+      x=rep(NA,N)
+      for(i in 1:N) x[i] = (1:length(P))[runif(1)<P][1]
+      data <- cbind(table(x)/N,prob)
+      data <- t(data)
+      
+      rownames(data) <- c("Empirique","Théorique")
+      
+      
+      count = c(sum(x==1), sum(x==2), sum(x==3), sum(x==4), sum(x==5), sum(x==6))
+      
+      dim(count) <- c(3,2)
+      
+      test = chisq.test(count, p =prob)
+      
+      Q <- function(occur)
+      {
+        sum = 0
+        count_row = rowSums(occur)
+        count_col = colSums(occur)
+        for (i in 1:nrow(occur))
+        {
+          for (j in 1:ncol(occur))
+          {
+            E = (count_row[i] * count_col[j])/N
+            sum = sum + ((occur[i,j] - E)^2)/E
+          }
+        }
+        return (sum)
+      }
+      
+      z = Q(count)
+      
+      
+      ddl = 2
+      alpha = 0.05
+      
+      
+      z_iter = c(z_iter, z)
+      lmao = c(lmao, z <= qchisq(1 - alpha, ddl))
+    }
+      
+    
+    H0_list <- cbind(H0_list,table(lmao)[1]/400)
+    print(H0_list)
+    print(N)
+  }
+  
+  H1_iter <- cbind(H1_iter, H0_list)
+}
+
+print(H1_iter)
+dim(H1_iter) <- c(5,3)
+print(H1_iter)
+
---
+++ Test.R
@@ -0,0 +1,44 @@
+N <- 1000
+z_iter <- c()
+lmao <- c()
+
+for (epoch in 1:400) {
+  set.seed(epoch)  # Ensure reproducibility per epoch
+  prob <- rep(1/6, 6)
+  
+  # Simulate N dice rolls
+  x <- sample(1:6, N, replace = TRUE)
+  
+  # Reshape counts into 3x2 contingency table
+  count <- matrix(tabulate(x, nbins = 6), nrow = 3, ncol = 2)
+  
+  # Custom Q function for chi-squared statistic
+  Q <- function(occur) {
+    sum_val <- 0
+    count_row <- rowSums(occur)
+    count_col <- colSums(occur)
+    total <- sum(occur)
+    
+    for (i in 1:nrow(occur)) {
+      for (j in 1:ncol(occur)) {
+        E <- (count_row[i] * count_col[j]) / total
+        sum_val <- sum_val + ((occur[i, j] - E)^2) / E
+      }
+    }
+    return(sum_val)
+  }
+  
+  z <- Q(count)
+  ddl <- (nrow(count) - 1) * (ncol(count) - 1)  # Correct degrees of freedom
+  alpha <- 0.05
+  
+  z_iter <- c(z_iter, z)
+  lmao <- c(lmao, z <= qchisq(1 - alpha, ddl))
+}
+
+hist(z_iter, freq = FALSE, main = "Histogram of Chi-squared Statistics")
+curve(dchisq(x, df = 2), add = TRUE, col = "red")  # Overlay theoretical density
+
+barplot(table(lmao)/400, names.arg = c("Reject H0", "Fail to Reject H0"),
+        ylab = "Proportion", main = "Test Outcomes")
+
